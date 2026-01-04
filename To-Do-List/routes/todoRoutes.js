76ff2e61aaf9bf7d98429dad47f4677a4eee5a48
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2026-01-04 18:41:40 +0530

    new added in route

--- To-Do-List/routes/todoRoutes.js
+++ To-Do-List/routes/todoRoutes.js
@@ -24,4 +24,33 @@ router.route('/:id')
 
 router.patch('/:id/toggle', toggleComplete);
 
-module.exports = router;
+// module.exports = router;
+
+
+const express = require('express');
+const router = express.Router();
+const {
+  getTodos,
+  getTodo,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  toggleComplete
+} = require('../controllers/todoController');
+const { protect } = require('../middleware/auth');
+
+// Protect all routes
+router.use(protect);
+
+router.route('/')
+  .get(getTodos)
+  .post(createTodo);
+
+router.route('/:id')
+  .get(getTodo)
+  .put(updateTodo)
+  .delete(deleteTodo);
+
+router.patch('/:id/toggle', toggleComplete);
+
+// module.exports = router;
--- To-Do-List/server.js
+++ To-Do-List/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI)
