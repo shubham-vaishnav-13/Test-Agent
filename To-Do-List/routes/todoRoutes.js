839d225d1b5afb43a93971dffcc1d137b1a1e7d6
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2026-01-04 19:06:46 +0530

    new added in route 2

--- To-Do-List/routes/todoRoutes.js
+++ To-Do-List/routes/todoRoutes.js
@@ -11,7 +11,7 @@ const {
 const { protect } = require('../middleware/auth');
 
 // Protect all routes
-router.use(protect);
+// router.use(protect);
 
 router.route('/')
   .get(getTodos)
@@ -24,33 +24,4 @@ router.route('/:id')
 
 router.patch('/:id/toggle', toggleComplete);
 
-// module.exports = router;
-
-
-const express = require('express');
-const router = express.Router();
-const {
-  getTodos,
-  getTodo,
-  createTodo,
-  updateTodo,
-  deleteTodo,
-  toggleComplete
-} = require('../controllers/todoController');
-const { protect } = require('../middleware/auth');
-
-// Protect all routes
-router.use(protect);
-
-router.route('/')
-  .get(getTodos)
-  .post(createTodo);
-
-router.route('/:id')
-  .get(getTodo)
-  .put(updateTodo)
-  .delete(deleteTodo);
-
-router.patch('/:id/toggle', toggleComplete);
-
-// module.exports = router;
+module.exports = router;
