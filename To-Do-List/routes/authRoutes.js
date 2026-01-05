e211c537138d6e73ac9eb0944b56966fde4722d2
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2026-01-05 16:13:25 +0530

    Some Fixes

--- To-Do-List/routes/authRoutes.js
+++ To-Do-List/routes/authRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
+const { logout, refreshToken, updateUser, forgotPassword, resetPassword } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
+router.post('/logout', protect, logout);
+router.post('/refresh', refreshToken);
+router.put('/update', protect, updateUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:resetToken', resetPassword);
 module.exports = router;
